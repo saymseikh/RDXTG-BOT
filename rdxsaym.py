61c7456acd19333e6721691131e9f856292c792b
SaymSeikh <123796426+saymseikh@users.noreply.github.com>
2023-02-16 20:49:31 +0600

    Update rdxsaym.py

--- rdxsaym.py
+++ rdxsaym.py
@@ -35,8 +35,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'Harshit ethic'
-username = 'harshitethic_bot'
+botname = 'RdxChatGPT-Bot'
+username = 'rdxchatgpt_bot'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
@@ -66,8 +66,8 @@ def start(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'RdxChatGPT-Bot'
+        username = 'rdxchatgpt_bot'
         update.message.reply_text('Hi')
         return 
     else:
@@ -93,16 +93,16 @@ def reset(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'RdxChatGPT-Bot'
+        username = 'rdxchatgpt_bot'
         update.message.reply_text('Bot has been reset, send a message!')
         return
     if tim == 1:
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'RdxChatGPT-Bot'
+        username = 'rdxchatgpt_bot'
         update.message.reply_text('Bot has been reset, send a message!')
         return 
     else:
@@ -128,8 +128,8 @@ def retry(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'RdxChatGPT-Bot'
+        username = 'rdxchatgpt_bot'
         update.message.reply_text('Send a message!')
         return 
     else:
@@ -189,8 +189,8 @@ def wait(bot, update, botname, username, new):
                 cache = None
                 qcache = None
                 user = ""
-                username = 'harshitethic_bot'
-                botname = 'Harshit Ethic'
+                username = 'rdxchatgpt_bot'
+                botname = 'RdxChatGPT-Bot'
                 update.message.reply_text('Timer has run down, bot has been reset to defaults.')
                 running = False
     else:
